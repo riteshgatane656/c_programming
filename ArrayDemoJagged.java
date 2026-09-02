56f816f412dd9e8edf359b19f019301f84837ac7
ritesh_gatane <254677979+riteshgatane@users.noreply.github.com>
2026-09-03 00:16:17 +0530

    Add files via upload

---
+++ ArrayDemoJagged.java
@@ -0,0 +1,26 @@
+class ArrayDemoJagged
+{
+    public static void main(String A[] )
+    { int Arr[][] = new int [3][]  ;
+      
+       Arr[0] = new int [4];
+
+        Arr[1] = new int [7];
+
+         Arr[2] = new int [5];
+
+       System.out.println(Arr.length);
+       System.out.println(Arr[0].length);
+       System.out.println(Arr[1].length);
+       System.out.println(Arr[2].length);
+    }
+}
+      
+
+      
+
+    
+
+    
+        
+
